d2e648ad6515a78f67e0148f1676884fee1c9a0b
Nisha-gif227 <csnisha21@gmail.com>
2021-09-19 17:16:26 +0530

    personal portfolio

---
+++ src/containers/Main.js
@@ -0,0 +1,107 @@
+/*import React from "react";
+import Header from "../components/header/Header";
+import Greeting from "./greeting/Greeting";
+import Skills from "./skills/Skills";
+import StackProgress from "./skillProgress/skillProgress";
+
+import Projects from "./projects/Projects";
+
+import Footer from "../components/footer/Footer";
+
+import Education from "./education/Education";
+import Top from "./topbutton/Top";
+
+import Profile from "./profile/Profile";
+import {StyleProvider} from "../contexts/StyleContext";
+
+import "./Main.scss";
+
+const Main = () => {
+  const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+
+  const changeTheme = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+    <div className={isDark ? "dark-mode" : null}>
+      <StyleProvider value={{isDark: isDark, changeTheme: changeTheme}}>
+        <Header />
+        <Greeting />
+        <Skills />
+        <StackProgress />
+        <Education />
+        
+        <Projects />
+        
+    
+        
+        <Profile />
+        <Footer />
+        <Top />
+      </StyleProvider>
+    </div>
+  );
+};*/
+import React, {Component} from "react";
+import Header from "../components/header/Header";
+import Greeting from "./greeting/Greeting";
+import Skills from "./skills/Skills";
+import StackProgress from "./skillProgress/skillProgress";
+// import WorkExperience from "./workExperience/WorkExperience";
+import Projects from "./projects/Projects";
+import StartupProject from "./StartupProjects/StartupProject";
+import Footer from "../components/footer/Footer";
+import Education from "./education/Education";
+import Top from "./topbutton/Top";
+import {StyleProvider} from "../contexts/StyleContext";
+import "./Main.scss";
+import Profile from "./profile/Profile";
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDark: false
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("isDark") === null) {
+      const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+      localStorage.setItem("isDark", darkPref.matches);
+    }
+    this.setState({isDark: JSON.parse(localStorage.getItem("isDark"))});
+  }
+  changeTheme = () => {
+    this.setState({isDark: !this.state.isDark}, () => {
+      localStorage.setItem("isDark", this.state.isDark);
+    });
+  };
+
+  render() {
+    return (
+      <div className={this.state.isDark ? "dark-mode" : null}>
+        <StyleProvider
+          value={{isDark: this.state.isDark, changeTheme: this.changeTheme}}
+        >
+          <Header />
+          <Greeting />
+          <Skills />
+          <StackProgress />
+          <Education />
+          {/* <WorkExperience /> */}
+          <Projects />
+          <StartupProject />
+          <Profile />
+          <Footer />
+          <Top />
+        </StyleProvider>
+      </div>
+    );
+  }
+}
+
+
+
---
+++ src/portfolio.js
@@ -0,0 +1,290 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Nisha Rani",
+  title: "Hi all, I'm Nisha",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/13Mp1JxL0Sk2NHR07onbe3-d-brmRggWH/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/Nisha-Rani749/",
+  linkedin: "https://www.linkedin.com/in/nisha-rani-641691202/",
+  gmail: "csnisha21@gmail.com",
+  instagram:"https://www.instagram.com/nisharani492/",
+  facebook: "https://www.facebook.com/profile.php?id=100031143469247",
+  
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+   
+    emoji("⚡ Making Progressive MERN Stack Web Applications from Start till the Deployment"),
+    emoji("⚡ Develop interactive Front end / User Interfaces for your web applications"),
+    emoji("⚡ Currently Learning React Native for Mobile App development")
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+   
+    {
+      skillName: "Git",
+      fontAwesomeClassname: "fab fa-git"
+    },
+   
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "The Hacking School",
+      logo: require("./assets/images/theHackingSchool.png"),
+      subHeader: "Full Stack Web Development",
+      duration: "january 2021 - August 2021",
+      desc: "Build many real world projects and got the first hand experience",
+      
+    },
+    {
+      schoolName: "Vinoba Bhave University",
+      logo: require("./assets/images/Vinoba-Bhave-University-VBU-1.png"),
+      subHeader: "Bachelor of Technology in Computer Science",
+      duration: "June 2018 - August 2021",
+      
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "80%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "70%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+//const workExperiences = {
+  //display: true, //Set it to true to show workExperiences Section
+  //experience: [
+   // {
+      //role: "Software Engineer",
+      //company: "Facebook",
+      //companylogo: require("./assets/images/facebookLogo.png"),
+      //date: "June 2018 – Present",
+      //desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      //descBullets: [
+        //"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        //"Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      //]
+    //},
+    //{
+      //role: "Front-End Developer",
+      //company: "Quora",
+      //companylogo: require("./assets/images/quoraLogo.png"),
+      //date: "May 2017 – May 2018",
+      //desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    //},
+    //{
+      //role: "Software Engineer Intern",
+      //company: "Airbnb",
+      //companylogo: require("./assets/images/airbnbLogo.png"),
+      //date: "Jan 2015 – Sep 2015",
+      //desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+   // }
+  //]
+//};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Saayahealth",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "http://saayahealth.com/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+  ]
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+
+ 
+
+// Blogs Section
+
+/*const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};*/
+
+// Talks Sections
+
+/*const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};*/
+
+// Podcast Section
+
+/*const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: true // Set false to hide this section, defaults to true
+};*/
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+91-9798171713",
+  email_address: "csnisha21@gmail.com"
+};
+
+// Twitter Section
+
+//const twitterDetails = {
+  //userName: "twitter", //Replace "twitter" with your twitter username without @
+  //display: true // Set true to display this section, defaults to false
+//};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  //workExperiences,
+  openSource,
+  bigProjects,
+  
+  contactInfo,
+  
+};
