ed747b645d97631e5b5d0ffefc725dbd4755ca58
Nisha-Rani749 <Nisha-Rani749@users.noreply.github.com>
2021-09-20 12:22:35 +0000

    Deploying to gh-pages from  @ d2e648ad6515a78f67e0148f1676884fee1c9a0b 🚀

---
+++ precache-manifest.f801164a37645ce66176c935e9394d7c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5083ba718363f8d8f65f1e99f64f7ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62a37a8fd47ddff87214",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "a76caa429d90b9d7b59a",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "58fc61d3914d26556063",
+    "url": "/static/css/main.2ee79ce7.chunk.css"
+  },
+  {
+    "revision": "e9eaa086481edeec6987",
+    "url": "/static/js/2.5cd1b301.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.5cd1b301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a37a8fd47ddff87214",
+    "url": "/static/js/3.4db6a5b8.chunk.js"
+  },
+  {
+    "revision": "a76caa429d90b9d7b59a",
+    "url": "/static/js/4.a0a24a5b.chunk.js"
+  },
+  {
+    "revision": "58fc61d3914d26556063",
+    "url": "/static/js/main.b7fb5c6f.chunk.js"
+  },
+  {
+    "revision": "141491cbfaaf9f826c97",
+    "url": "/static/js/runtime-main.ccba6fa6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8a29c8ad6b8df860d6bc9dfb26d504a9",
+    "url": "/static/media/Vinoba-Bhave-University-VBU-1.8a29c8ad.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "541254fcb1c051e7c88141de2a226be5",
+    "url": "/static/media/theHackingSchool.541254fc.png"
+  }
+]);
